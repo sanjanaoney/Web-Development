a46ba4579de9c39515ce2630b760d1f42f9f8681
sanjanaoney <iamoney012@gmail.com>
2026-05-09 23:53:06 +0600

    js problem solving

---
+++ JavaScript/js_problems_part2/max2.js
@@ -0,0 +1,22 @@
+const jim=56;
+const tim=89;
+const kim=68;
+
+if(jim> tim && jim>kim)
+{
+    console.log('jim is the ultimate boss')
+}
+else if( tim>jim && tim>kim){
+    console.log('Tim is the boss')
+}
+else{
+    console.log('kim kardashian')
+}
+
+function maxOfThree(num1,num2,num3)
+{
+  //logic same
+}
+
+const max=Math.max(12,1,56,5,65,8,1,99,2)
+console.log('max using Math.max', max)
