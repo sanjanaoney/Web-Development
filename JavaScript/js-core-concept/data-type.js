f025e50a90a4c7e4efe391ccbfc872990785e3bc
Sanjana <iamoney012@gmail.com>
2026-08-29 00:40:49 +0600

    js-core

---
+++ JavaScript/js-core-concept/data-type.js
@@ -0,0 +1,23 @@
+//js is a dynamic typed programming language
+let data=4;
+data='apple';
+data=true;
+data={name: 'bob', friend: 'alice'}
+console.log(typeof data)
+data=[445]
+data=undefined
+data=null
+console.log(typeof data);
+
+//js: primitive data type
+const price=50;
+const name='kurkure'
+const isPoor=false;
+let isLocal;//undefined
+console.log(isLocal)
+const merit=null;
+
+//non-primitive data type
+const student={name:'badal', location: 'dhaka' ,subject:'pharmacy'}
+const friends=['tom','bob','jack','rock']
+console.log(friends)
