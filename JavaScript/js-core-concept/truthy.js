ced76975a5057b4429a13ee2cda13b2170c7e5de
Sanjana <iamoney012@gmail.com>
2026-09-10 03:21:43 +0600

    back-again

---
+++ JavaScript/js-core-concept/truthy.js
@@ -0,0 +1,31 @@
+let data;
+data=0;
+data=''; //empty string is falsy
+data=' '; //has an white space
+data='0';
+data=false;
+data=true;
+data=null;
+data={};
+data=[];
+data=false;
+
+console.log('value of data',data)
+if(data){
+    console.log('value of data truethy')
+}
+else{
+    console.log(data,'is falsy')
+}
+
+//if i need to capture the falsy value to go inside if block
+//use logical not
+if(!data){
+console.log('inside if with a falsy value')
+}
+
+//capture all positive value
+//capture any value to boolean
+if(!data===true){
+    console.log('only true inside the double not')
+}
