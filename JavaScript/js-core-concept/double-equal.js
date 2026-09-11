1613fc73031ace4b0bde9886d38c55fe3a72bdb2
Sanjana <iamoney012@gmail.com>
2026-09-11 20:45:49 +0600

    concepts

---
+++ JavaScript/js-core-concept/double-equal.js
@@ -0,0 +1,6 @@
+//type coersion
+console.log(2==2);
+console.log(2=='2'); //triple eqaul hole false dito
+console.log(true==1); ///string ke number e convert kore,boolean ke number e convert korbe then check korbe
+console.log(null==undefined) //will return true
+console.log(NaN===NaN)
