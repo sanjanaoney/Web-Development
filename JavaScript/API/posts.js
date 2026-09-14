ac2b0a08dece495455ace238ba5ad7ecb0e1a5ff
Sanjana <iamoney012@gmail.com>
2026-09-14 23:25:47 +0600

    displaying-post-card

--- JavaScript/API/posts.js
+++ JavaScript/API/posts.js
@@ -8,35 +8,30 @@ fetch(url)
 })
 };
 
-//recieved array of objects
+// {
+//     "userId": 10,
+//     "id": 99,
+//     "title": "temporibus sit alias delectus eligendi possimus magni",
+//     "body": "quo deleniti praesentium dicta non quod\naut est molestias\nmolestias et officia quis nihil\nitaque dolorem quia"
+// }
+
 const displayPosts=(posts)=>{
-    // console.log(posts);
-    // for(let i=0;i<posts.length;i++){
-    //     console.log(posts[i]);
-    // }
 
-    // for(let post of posts){
-    //     console.log(post)
-    // }
-//to show the data in the ui first
-    //1.get the container
-
-    const postContainer=document.getElementById("post-container")
-    // console.log(postContainer)
-
-    posts.forEach((post)=>{
-        console.log(post.title);
-
-        //2.create HTML element
-
-        const li=document.createElement("li");
-        li.innerText=post.title
-        console.log(li);
-
-
-        //3. add li into container
-        postContainer.appendChild(li);
+    //1.get the container and empty the container
+    const postsContainer=document.getElementById("post-container")
+    postsContainer.innerHTML="";
+    posts.forEach(post=>{
+        //2. create element
+        const postCard=document.createElement("div");
+        postCard.innerHTML=`
+        <div class="post-card">
+<h2>${post.title}</h2>
+<p>${post.body}</p>
+</div>
+        
+        `
+        //3.add to the container
+        postsContainer.append(postCard)
     })
-
-
-}
+};
+loadPost();
