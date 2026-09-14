4682e8b1c10d712b4e96ec5f75f3b1fc136904bd
Sanjana <iamoney012@gmail.com>
2026-09-15 00:47:18 +0600

    displaying-todo-list

---
+++ JavaScript/API/todo.js
@@ -0,0 +1,41 @@
+const loadTodo=()=>{
+    const url="https://jsonplaceholder.typicode.com/todos"
+    fetch(url)
+    .then((res)=>res.json())
+    .then((data)=>displayTodo(data));
+};
+
+// {
+//     "userId": 10,
+//     "id": 197,
+//     "title": "dignissimos quo nobis earum saepe",
+//     "completed": true
+// }
+
+const displayTodo=(todos)=>{
+// console.log(todos);
+//1.
+const todoContainer=document.getElementById("todo-container")
+todoContainer.innerHTML="";
+
+todos.forEach((todo)=>{
+    console.log(todo);
+
+    const todoCard=document.createElement("div");
+    todoCard.innerHTML=`
+    <div class="todo-card">
+    <p>${todo.completed==true ?
+         `<i class="fa-regular fa-square-check"></i>`
+        :
+         `<i class="fa-solid fa-square-check"></i>`} 
+         </p>
+    <h4>${todo.title} </h4>
+
+    </div>
+    `;
+
+
+    todoContainer.append(todoCard);
+})
+};
+loadTodo();
